e9f2f42c51a008796334a9001997ca0f60fafc22
Wiktor <wiktor.muraszko@wp.pl>
2021-04-17 19:20:32 +0200

    big update

--- Assets/Scripts/BG/BG_change.cs
+++ Assets/Scripts/BG/BG_change.cs
@@ -1,18 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BG_change : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public int next_scene;
     // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D other)
     {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            print("triger");
+            SceneManager.LoadScene(next_scene, LoadSceneMode.Single);
+        }
         
     }
 }
